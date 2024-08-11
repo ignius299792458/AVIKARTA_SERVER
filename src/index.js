6bcc93ad5dd37978d1b0fb8f105e8a3035e936a0
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-08-11 17:51:46 +0545

    build script

--- src/index.js
+++ src/index.js
@@ -5,13 +5,23 @@ import app from './app.js';
 import mongodbApi from '../src/DB/connect.db.js';
 
 // ------------------------------ setting MONGOOSE database and PORT listen ------------------------------
-// const serverHost = '192.168.16.151' || 'localhost'; //metalogic2g
+const serverHost = '192.168.16.151' || 'localhost'; //metalogic2g
 // const serverHost = '192.168.1.114' || 'localhost'; //metalogic4g
 // const serverHost = '192.168.18.8' || 'localhost'; //room
 
-// const serverHost = 'localhost';
 // const serverHost = '192.168.1.165' || 'localhost'; //metalogic2g
 // console.log('server_host: ', serverHost);
+// mongodbApi()
+//     .then(() => {
+//         app.listen(process.env.PORT, serverHost, () => {
+//             console.log(`-->R: http://${serverHost}:${process.env.PORT}`);
+//         });
+//     })
+//     .catch((error) => {
+//         console.log('-->E: DB connection FAILED !! ', error, '@src/index.js');
+//     });
+
+// deploy
 mongodbApi()
     .then(() => {
         app.listen(process.env.PORT, () => {
